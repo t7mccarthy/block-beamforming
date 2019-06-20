eb3653bf0a4e6743e87498539888ca0bd8aa2011
t7mccarthy <thomasmccarthy@college.harvard.edu>
2019-06-20 14:53:13 -0700

    done with 1d

--- 1D_blocks.py
+++ 1D_blocks.py
@@ -1,6 +1,7 @@
 import cmath
 import math
 import matplotlib.pyplot as plt
+from statistics import mean
 from tabulate import tabulate
 
 
@@ -20,25 +21,19 @@ class Antenna:
         self.amplitude = x / math.cos(self.phase)
     def get_phase_amp(self):
         return (math.degrees(self.phase), self.amplitude)
-    def get_excitation(self):
-        return self.excitation
 
 class Block:
     def __init__(self, p, s = lmbda / 2):
         self.pos = p
         self.spacing = s
         self.antennas = (Antenna(), Antenna())
-    def get_pos(self):
-        return self.pos
-    def get_spacing(self):
-        return self.spacing
-    def get_antennas(self):
-        return self.antennas
     def set_antennas(self, I):
         self.antennas[0].set_values(I[0])
         self.antennas[1].set_values(I[1])
     def get_antenna_vals(self):
-        return [self.antennas[i].get_phase_amp() for i in range(2)]
+        return ([self.antennas[0].amplitude, self.antennas[1].amplitude], [self.antennas[0].phase, self.antennas[1].phase])
+    def get_excitation(self, i):
+        return self.antennas[i].excitation
 
 class BlockArray:
     def __init__(self, ps, t):
@@ -47,73 +42,46 @@ class BlockArray:
         self.num_blocks = len(self.blocks)
         a_pos = [0] * (2 * self.num_blocks)
         for b in range(self.num_blocks):
-            center = self.blocks[b].get_pos()
-            offset = self.blocks[b].get_spacing() / 2
+            center = self.blocks[b].pos
+            offset = self.blocks[b].spacing / 2
             a_pos[b * 2] = center - offset
             a_pos[(b * 2) + 1] = center + offset
         self.a_pos = a_pos
+        self.afs = False
 
     def get_array_factor(self, theta):
         sum = 0
-        I_0 = self.blocks[0].get_antennas()[0].get_excitation()
+        I_0 = self.blocks[0].get_excitation(0)
         for a in range(self.num_blocks * 2):
-            I_n = self.blocks[int(a / 2)].get_antennas()[a % 2].get_excitation()
+            I_n = self.blocks[int(a / 2)].get_excitation(a % 2)
             sum += (I_n / I_0) * cmath.exp(1j * k * self.a_pos[a] * math.cos(theta))
         return sum
 
-    def get_solution(self):
+    def calc_afs(self):
+        if not self.afs:
+            y = [0] * 181
+            for i in range(181):
+                y[i] = abs(self.get_array_factor(math.radians(i)))
+            self.afs = y[:]
+
+    def solve(self):
         for b in range(self.num_blocks):
-            I = [cmath.exp(1j * -k * self.a_pos[(2 * b) + i]
-                * math.cos(self.target)) for i in range(2)]
+            I = [cmath.exp(1j * -k * self.a_pos[(2 * b) + i] * math.cos(self.target)) for i in range(2)]
             self.blocks[b].set_antennas(I)
 
     def output_results(self):
-        # amps = [0] * self.num_blocks * 2
-        # phases = [0] * self.num_blocks * 2
-        # for b in range(0, self.num_blocks * 2, 2):
-        #     a1, a2 = self.blocks[int(b / 2)].get_antenna_vals()
-        #     phases[b], amps[b] = a1
-        #     phases[b + 1], amps[b + 1] = a2
-        #
-        # block_nums = list(sum(zip([str(i + 1) for i in range(self.num_blocks)], [""] * self.num_blocks), ())[:-1]) + ['']
-        # # output antenna configurations
-        # print ('\n' + '\033[1m' + 'Required Antenna Configurations:' + '\033[0m\n')
-        # array_config = zip(*[block_nums, amps, phases])
-        # print (tabulate(array_config, headers=['Block #', 'Amplitude', 'Phase'], tablefmt='orgtbl') + '\n')
-
-        amps = [0] * self.num_blocks
-        phases = [0] * self.num_blocks
         print ('\n' + '\033[1m' + 'Required Antenna Configurations:' + '\033[0m\n')
         for b in range(self.num_blocks):
-            a1, a2 = self.blocks[b].get_antenna_vals()
-            phase1, amp1 = a1
-            phase2, amp2 = a2
-            amps[b] = (amp1, amp2)
-            phases[b] = (phase1, phase2)
+            amps, phases = self.blocks[b].get_antenna_vals()
             print ('Block ' + str(b + 1) + ':')
-            array_config = zip(*[[amp1, amp2], [phase1, phase2]])
-            print (tabulate(array_config, headers=['Amplitude', 'Phase'], tablefmt='orgtbl') + '\n')
-
-        # # get array factor at each target angle
-        # target_results = []
-        # for target in targets:
-        #     target_results.append(abs(get_array_factor(math.radians(target))))
-        # closest_peaks = []
-        # for angle in closest:
-        #     closest_peaks.append(abs(get_array_factor(math.radians(angle))))
-        #
-        # # output results
-        # print ('\n\033[1m Resulting Beamform:\033[0m\n')
-        # print ('--- Best uniform peak array factor: ' + str(M / T))
-        # print ('--- Runtime: ' + str(runtime) + ' seconds\n')
-        # results = zip(*[[x + 1 for x in range(T)], targets, closest, target_results, closest_peaks])
-        # print (tabulate(results, headers=['Target #', 'Target Angle', 'Closest Sample', 'Array Factor', 'AF at Closest'], tablefmt='orgtbl') + '\n')
-        # diffs = [abs(targets[i] - closest[i]) for i in range(T)]
-        # print ('--- Mean difference target vs. closest: ' + str(sum(diffs)/T))
-        # print ('--- Max difference target vs. closest: ' + str(max(diffs)))
-        # print ('--- Mean array factor at target: ' + str(sum(target_results)/T))
-        # print ('--- Max array factor at target: ' + str(max(target_results)))
-        # print ('--- Min array factor at target: ' + str(min(target_results)) + '\n')
+            array_config = zip(*[[1, 2], amps, phases])
+            print (tabulate(array_config, headers=['Antenna #', 'Amplitude', 'Phase'], tablefmt='orgtbl') + '\n')
+        print ('\n\033[1m Resulting Beamform:\033[0m\n')
+        print ('--- Best expected array factor: ' + str(self.num_blocks * 2))
+        print ('--- Array factor at target: ' + str(abs(self.get_array_factor(self.target))))
+        self.calc_afs()
+        print ('--- Mean array factor: ' + str(mean(self.afs)))
+        print ('\nFINISHED\n')
 
     def visualize(self, show_block_positions=False):
         if show_block_positions:
@@ -121,11 +89,8 @@ class BlockArray:
             plt.title('Antenna Positions')
             plt.xlabel('Position')
             plt.show()
-
-        y = [0] * 181
-        for i in range(181):
-            y[i] = abs(self.get_array_factor(math.radians(i)))
-        plt.plot(range(181), y)
+        self.calc_afs()
+        plt.plot(range(181), self.afs)
         plt.title('Block-Generated Beamform')
         plt.xlabel('Angle from Array (Degrees)')
         plt.ylabel('Array Factor')
@@ -133,8 +98,8 @@ class BlockArray:
 
 
 
-# array1 = BlockArray([-20, -lmbda, -100, -90, -75, -60, 1.3 * lmbda, 30, 35, 40, 50], math.radians(125))
-array1 = BlockArray([-0, -lmbda, lmbda, -2 * lmbda, 2 * lmbda, -3 * lmbda, 3 * lmbda, -4*lmbda, 4*lmbda, -5*lmbda, 5*lmbda], math.radians(125))
-array1.get_solution()
+array1 = BlockArray([-20, -lmbda, -100, -90, -75, -60, 1.3 * lmbda, 30, 35, 40, 50], math.radians(125))
+# array1 = BlockArray([-0, -lmbda, lmbda, -2 * lmbda, 2 * lmbda, -3 * lmbda, 3 * lmbda, -4*lmbda, 4*lmbda, -5*lmbda, 5*lmbda], math.radians(125))
+array1.solve()
 array1.output_results()
 array1.visualize(True)
