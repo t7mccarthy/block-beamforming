3c98d582495d6429076ca7f3fcec614fb2312a46
t7mccarthy <thomasmccarthy@college.harvard.edu>
2019-06-20 17:34:38 -0700

    done with 2d block array

--- 2D_blocks.py
+++ 2D_blocks.py
@@ -34,7 +34,7 @@ class Block:
         for i in range(4):
             self.antennas[i].set_values(I[i])
     def get_antenna_vals(self):
-        return ([(a.phase, a.amplitude) for a in self.antennas])
+        return ([(a.amplitude, a.phase) for a in self.antennas])
     def get_excitation(self, i):
         return self.antennas[i].excitation
 
@@ -55,15 +55,6 @@ class BlockArray:
         self.a_pos = a_pos
         self.afs = False
 
-    def get_array_factor(self, theta, phi):
-        sum = 0
-        I_0 = self.blocks[0].get_excitation(0)
-        for a in range(self.num_blocks * 2):
-            I_n = self.blocks[int(a / 2)].get_excitation(a % 2)
-            sum += (I_n / I_0) * cmath.exp(1j * k * self.a_pos[a] * math.cos(theta))
-        return sum
-
-
     def get_array_factor (self, theta, phi):
         sum = 0
         I_00 = self.blocks[0].get_excitation(0)
@@ -77,27 +68,28 @@ class BlockArray:
             afs = [[0] * 360 for i in range(90)]
             for t in range(90):
                 for p in range(360):
-                    y[i] = abs(self.get_array_factor(math.radians(t), math.radians(p)))
+                    afs[t][p] = abs(self.get_array_factor(math.radians(t), math.radians(p)))
             self.afs = afs[:]
 
-    # def solve(self):
-    #     for b in range(self.num_blocks):
-    #         I = [cmath.exp(1j * -k * self.a_pos[(2 * b) + i] * math.cos(self.target)) for i in range(2)]
-    #         self.blocks[b].set_antennas(I)
+    def solve(self):
+        for b in range(self.num_blocks):
+            I = [cmath.exp(1j * -k * math.sin(self.target[0]) * (self.a_pos[(4 * b) + i][0] * math.cos(self.target[1]) + self.a_pos[(4 * b) + i][1] * math.sin(self.target[1]))) for i in range(4)]
+            self.blocks[b].set_antennas(I)
+            print (I)
 
-    # def output_results(self):
-    #     print ('\n' + '\033[1m' + 'Required Antenna Configurations:' + '\033[0m\n')
-    #     for b in range(self.num_blocks):
-    #         amps, phases = self.blocks[b].get_antenna_vals()
-    #         print ('Block ' + str(b + 1) + ':')
-    #         array_config = zip(*[[1, 2], amps, phases])
-    #         print (tabulate(array_config, headers=['Antenna #', 'Amplitude', 'Phase'], tablefmt='orgtbl') + '\n')
-    #     print ('\n\033[1m Resulting Beamform:\033[0m\n')
-    #     print ('--- Best expected array factor: ' + str(self.num_blocks * 2))
-    #     print ('--- Array factor at target: ' + str(abs(self.get_array_factor(self.target))))
-    #     self.calc_afs()
-    #     print ('--- Mean array factor: ' + str(mean(self.afs)))
-    #     print ('\nFINISHED\n')
+    def output_results(self):
+        print ('\n' + '\033[1m' + 'Required Antenna Configurations:' + '\033[0m\n')
+        for b in range(self.num_blocks):
+            (a0, p0), (a1, p1), (a2, p2), (a3, p3) = self.blocks[b].get_antenna_vals()
+            print ('Block ' + str(b + 1) + ':')
+            array_config = zip(*[range(4), [a0, a1, a2, a3], [p0, p1, p2, p3]])
+            print (tabulate(array_config, headers=['Antenna #', 'Amplitude', 'Phase'], tablefmt='orgtbl') + '\n')
+        print ('\n\033[1m Resulting Beamform:\033[0m\n')
+        print ('--- Best expected array factor: ' + str(self.num_blocks * 4))
+        print ('--- Array factor at target: ' + str(abs(self.get_array_factor(self.target[0], self.target[1]))))
+        self.calc_afs()
+        print ('--- Mean array factor: ' + str(mean([item for sublist in self.afs for item in sublist])))
+        print ('\nFINISHED\n')
 
     def visualize(self, show_block_positions=False):
         if show_block_positions:
@@ -117,26 +109,19 @@ class BlockArray:
         R = np.array(r)
         X = R * np.sin(THETA) * np.cos(PHI)
         Y = R * np.sin(THETA) * np.sin(PHI)
-        Z = -abs(R * np.cos(THETA))
+        Z = abs(R * np.cos(THETA))
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1, projection='3d')
         plot = ax.plot_surface(X, Y, Z)
-
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
-        ax.set_zlabel('Z Label')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.title('Block-Generated Beamform')
         plt.show()
-        # self.calc_afs()
-        # plt.plot(range(181), self.afs)
-        # plt.title('Block-Generated Beamform')
-        # plt.xlabel('Angle from Array (Degrees)')
-        # plt.ylabel('Array Factor')
-        # plt.show()
 
 
-
-# array1 = BlockArray([(0, 0), (10, 8), (-17, 20), (-7, 0), (4, -25)], math.radians(30), math.radians(120))
-array1 = BlockArray([(lmbda/2, lmbda/2), (-lmbda/2, -lmbda/2), (lmbda/2, -lmbda/2), (-lmbda/2, lmbda/2)], math.radians(30), math.radians(120))
-# array1.solve()
-# array1.output_results()
+array1 = BlockArray([(0, 0), (10, 8), (-17, 20), (-7, 0), (4, -25), (-5, -17), (-1, 17)], math.radians(30), math.radians(0))
+# array1 = BlockArray([(lmbda, lmbda), (-lmbda, -lmbda), (lmbda, -lmbda), (-lmbda, lmbda), (lmbda, 0), (-lmbda, 0), (0, lmbda), (0, -lmbda), (0, 0)], math.radians(30), math.radians(30))
+array1.solve()
+array1.output_results()
 array1.visualize(True)
